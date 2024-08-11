bc278a473715d3e947b4e81cd1e7fddea52fc5f8
TempestFamily <tempestfamilylosangeles@gmail.com>
2024-08-11 08:25:14 +0300

    Add files via upload

---
+++ js/zoomimage.js
@@ -0,0 +1,19 @@
+const zoomLevel = 10; // Установите здесь нужное значение зума
+    let isZoomed = false; // Переменная для отслеживания состояния зума
+
+    function zoomImage(e) {
+        const img = document.getElementById('myImage');
+
+        if (!isZoomed) {
+            const rect = img.getBoundingClientRect(); // Получить размеры и положение изображения
+            const x = e.clientX - rect.left; // Координата X нажатия
+            const y = e.clientY - rect.top; // Координата Y нажатия
+
+            img.style.transformOrigin = `${x}px ${y}px`; // Установить точку зума
+            img.style.transform = `scale(${zoomLevel})`; // Применить зум
+        } else {
+            img.style.transform = 'scale(1)'; // Вернуться к обычному размеру
+        }
+
+        isZoomed = !isZoomed; // Меняем состояние зума
+    }
